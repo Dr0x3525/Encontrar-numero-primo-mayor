29305512489462ea33feaa8b55c60e0334c40be3
Dr0x3525 <151526410+Dr0x3525@users.noreply.github.com>
2024-12-13 00:30:52 -0500

    Add files via upload

--- NumerosPrimos.js
+++ NumerosPrimos.js
@@ -1,45 +1,85 @@
+
+//informacion de que hace el programa
 console.log("Aplicacion de numeros primos menores");
 console.log("Características de un número primo:");
 console.log("1.El número debe ser mayor que 1");
 console.log("2.El número solo debe ser divisible entre 1 y él mismo (sin dejar residuo).");
 console.log("Si el numero es decimal se tomara el valor aproximado")
 
+
+//inicializacion de variables
 let numero =  prompt("Escribe un numero")
 let numeroentero = parseInt(numero)
 
-console.log(typeof(numero))
 
-if (numeroentero === 1) {
-    console.log("el numero 1 no es valido")
+
+
+//funciones
+
+//funcion verificar que un numero sea valido
+
+function verificarnumerovalido() {
+    //console.log(typeof(numeroentero))
+    //console.log(typeof(numero))
+    //console.log(numeroentero)
+
+    if (isNaN(numeroentero) || numero.includes(".") || numeroentero <= 0 ){
+        console.log(numero + " no es un numero entero")
+        return false
+
+    } else {
+        console.log("Numero correcto")
+        return true
+    }
+
 }
 
-if (numeroentero === 2) {
-    numeroentero = 1
-    console.log("el numero 2 es el primo mas pequeño")
+// Función verificar si el número es 1
+
+function verificarEsUno() {
+    if (numeroentero === 1) {
+        console.log("el numero 1 no es valido por las reglas anteriormente mencionadas")
+    
+    return false    
+    }
+
+    else {
+        return true;
+    }
+
 }
 
-if (Number.isInteger(numeroentero) && numeroentero > 0) {
+// Función verificar si el número es 2
 
+function verificarEsDos() {
+    if (numeroentero === 2) {
+        console.log("el numero 2 es el primo mas pequeño por ende no hay menores que el XD")
+        return false    
+    }
 
+    else {
+        return true;
+    }
+}
 
-        for (let dividendo = numeroentero-1; 1 <= dividendo; dividendo--) {
+function encontrarNumerosPrimosMenores() {  
+    for (let dividendo = numeroentero-1; dividendo >= 2; dividendo--) {
 
-        let boton = false
+        let esCompuesto = false;
     
     
         for (let divisior = dividendo-1; 1< divisior; divisior--) {
             //console.log("numero o "+ divisior)
             
-            residuo = dividendo%divisior
-            //10
+            let residuo = dividendo%divisior
             //console.log(residuo)
     
     
     
-            if (residuo == 0) {
-                boton = true
+            if (residuo === 0) {
+                esCompuesto = true
+                divisior = 1
                 //console.log("este es "+ boton)
-    
             }
     
     
@@ -49,24 +89,34 @@ if (Number.isInteger(numeroentero) && numeroentero > 0) {
     
         //console.log("esteboton"+ boton)
     
-        if (boton == false) {
+        if (esCompuesto === false) {
             if (dividendo != 1) {
                 console.log("el numero "+ dividendo + " es un primo menor que "+ numeroentero)
             } else {
-                boton = true
+                esCompuesto = true
 
             }
 
         }
     
     }
+}
+//fin funciones
 
+verificarnumerovalido(numeroentero);
+
+if (numerovalido === true) {
+
+    //verifica casos especiales
+    verificarEsUno(numeroentero)
+    verificarEsDos(numeroentero)
+
+
+        //si todo es correcto empieza a iterar
+
+    if (numerovalido === true) {
+        encontrarNumerosPrimosMenores(numeroentero)
+    }
 
 }
-
-else {
-    console.log(numero + " no es un numero entero")
-}
-
-
 
